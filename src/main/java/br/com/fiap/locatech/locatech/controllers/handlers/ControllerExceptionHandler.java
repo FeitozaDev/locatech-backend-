fd903453f1e9f6f62a5fe15e226e1b044b4590d0
Lucas Feitoza <lucassilva202414@outlook.com>
2026-03-30 20:54:33 -0300

    Atualização : handler

--- src/main/java/br/com/fiap/locatech/locatech/controllers/handlers/ControllerExceptionHandler.java
+++ src/main/java/br/com/fiap/locatech/locatech/controllers/handlers/ControllerExceptionHandler.java
@@ -1,11 +1,15 @@
 package br.com.fiap.locatech.locatech.controllers.handlers;
-
 import br.com.fiap.locatech.locatech.dtos.ResourceNotFoundDTO;
+import br.com.fiap.locatech.locatech.dtos.ValidationErrorDTO;
 import br.com.fiap.locatech.locatech.services.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @ControllerAdvice
 public class ControllerExceptionHandler {
@@ -15,5 +19,16 @@ public class ControllerExceptionHandler {
         var status = HttpStatus.NOT_FOUND;
         return ResponseEntity.status(status.value())
                 .body(new ResourceNotFoundDTO(e.getMessage(), status.value()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationErrorDTO> handlerMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        var status = HttpStatus.BAD_REQUEST;
+        List<String> erros = new ArrayList<String>();
+        for (var erro : e.getBindingResult().getFieldErrors()) {
+            erros.add(erro.getField() + " : " + erro.getDefaultMessage());
+        }
+        return ResponseEntity.status(status.value())
+                .body(new ValidationErrorDTO(erros, status.value()));
     }
 }
--- src/main/java/br/com/fiap/locatech/locatech/dtos/AluguelRequestDTO.java
+++ src/main/java/br/com/fiap/locatech/locatech/dtos/AluguelRequestDTO.java
@@ -1,10 +1,17 @@
 package br.com.fiap.locatech.locatech.dtos;
 
+import jakarta.validation.constraints.NotNull;
+
 import java.time.LocalDate;
 
 public record AluguelRequestDTO(
+
+        @NotNull (message = " o Id da pessoa não pode ser Nulo.")
         Long pessoaId,
+
+        @NotNull (message = " o Id do veiculo não pode ser Nulo.")
         Long veiculoId,
+
         LocalDate dataInicio,
         LocalDate dataFim) {
 }
